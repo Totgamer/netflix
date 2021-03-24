ec97449c8e6e46fc5958cc625332a8d2aca3c39c
Marco Molenaar <532548@student.glu.nl>
2021-03-24 14:41:02 +0100

    netflix cards
    Co-authored-by: beau-vr <beau-vr@users.noreply.github.com>

--- netflix/src/components/navigation/Home.jsx
+++ netflix/src/components/navigation/Home.jsx
@@ -3,12 +3,26 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="Container">
-            <div className="title">
-                <h4>Home</h4>
-            </div>
-            <div className="desc">
-                <p>Some description of home...</p>
-            </div>
+            <section className="header Home-main">
+                <div className="title">
+                    <h4>Home</h4>
+                </div>
+                <div className="desc">
+                    <p>Some description of home...</p>
+                </div>
+            </section>
+            <section id="movies">
+                <ul className="items">
+                    <li>
+                        <div className="bg-img" style={{backgroundImage: `url("img/cover.jpg")`}}></div>
+                        <a href="#">
+                            <div className="content">
+                                <h2>Orange is the new black</h2>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </section>
         </div>
     )
 }
--- netflix/src/components/navigation/NavBar.jsx
+++ netflix/src/components/navigation/NavBar.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom'; 
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const NavBar = () => {
     return(
         <nav>
-            <div className="container">
-                <a className="main-logo">Netflix</a>
+            <div className="Navbar">
+                <Link to="/" className="main-logo"><img src="logo.png" alt="logo"/></Link>
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/series">Series</Link></li>
                     <li><Link to="/films">Films</Link></li>
                     <li><NavLink to="/newandpopulair">New And Populair</NavLink></li>
                     <li><NavLink to="/mylist">My List</NavLink></li>
+                </ul>
+                
+                <ul> 
+                    <li>
+                        <NavLink to=""><FontAwesomeIcon icon={faSearch} /></NavLink>
+                    </li>
                 </ul>
             </div>
         </nav>
